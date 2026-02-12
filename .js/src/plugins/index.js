8f7797ab8fa5ebf4a52f1bccbed44baab7fb2a57
Duc Phan <phanducsjsu@gmail.com>
2026-02-11 23:03:03 -0800

    chore: Publish Plugins

---
+++ .js/src/plugins/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var wtrlab_1 = __importDefault(require("@plugins/english/wtrlab"));
+var PLUGINS = [wtrlab_1.default];
+exports.default = PLUGINS;
